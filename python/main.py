41c7aac15bbec77f628ac8cf43ddbde2ff7b414e
unknown <jaseystudios@gmail.com>
2023-04-27 12:50:33 -0400

    update

--- python/main.py
+++ python/main.py
@@ -1,3 +1,18 @@
 class Actor:
-	def __init__(self):
+	def __init__(self, name):
 		self.hp = 10
+		self.name = name
+		self.mp = 10
+
+class Item:
+	def __init__(self, name):
+		self.name = name
+		self.hp_effect = 0
+		self.mp_effect = 0
+
+class Party:
+	def __init__(self):
+		self.actors=[]
+		self.inventory = []
+	def addMember(self, actor):
+		self.actors.append(actor)
